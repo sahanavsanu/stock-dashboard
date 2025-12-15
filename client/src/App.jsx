7a05a51bb052f70dcf7921b72aa41e0332c802ed
sahana <Sahanasanu455@gmail.com>
2025-12-15 23:34:09 +0530

    Debug blank page render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-
 export default function App() {
-  const [email, setEmail] = useState(null);
-
-  if (!email) {
-    return <Login onLogin={setEmail} />;
-  }
-
-  return <Dashboard email={email} />;
+  return (
+    <div style={{ padding: 40, color: "white" }}>
+      APP IS RENDERING
+    </div>
+  );
 }
