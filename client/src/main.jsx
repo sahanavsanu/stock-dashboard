96ca8e6e859530f4475545f7511df26b631652ca
sahana <Sahanasanu455@gmail.com>
2025-12-15 23:39:14 +0530

    Fix main.jsx root render

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
 
-const root = document.getElementById("root");
+const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
